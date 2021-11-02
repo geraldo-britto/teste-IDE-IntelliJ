5a046d0658bd18345a6e2bf3571cb6944adb91f0
Geraldo Britto <geraldo.britto@fatec.sp.gov.br>
2021-11-01 23:56:36 -0300

    First Commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato("Miu", "Amarelo", 5);
+
+        System.out.println(gato);
+
+        /*
+        int a = 5;
+        int b = 2;
+        System.out.println("Soma: " + (a+b) );
+        */
+
+    }
+}
